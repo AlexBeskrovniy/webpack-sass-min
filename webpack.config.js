f3df0cf135294701c27952cc44c76ba45989402e
AlexBlacksmith <unbloody.alex@gmail.com>
2022-10-04 07:58:38 +0500

    Added html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,21 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/styles.scss',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
         clean: true
     },
     mode: 'development',
     devServer: {
         hot: true,
+        open: true,
         port: 7777
     },
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
@@ -21,6 +26,10 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
     },
